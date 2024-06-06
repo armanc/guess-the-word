f117e40dfd3c37336e88bd13c8423c6f7b6d7988
Armands Baris <armands.baris@gmail.com>
2024-06-07 00:05:33 +0300

    Primitive working version

--- index.ts
+++ index.ts
@@ -1,1 +1,55 @@
-console.log("test")
+const prompts = require('prompts');
+
+(async () => {
+    const word: string = 'codelex';
+    const wordParts: string[] = word.split('');
+
+    let targetWord: string[] = '_'.repeat(word.length).split('');
+
+    let guesses: number = 0;
+    const maxGuesses: number = word.length+3;
+
+    console.log('Vārds: '+targetWord.join(' '))
+
+    while (guesses < maxGuesses) {
+
+    
+        const response = await prompts({
+            type: 'text',
+            name: 'letter',
+            message: 'Enter letter: ',
+          });
+
+        console.log(wordParts.includes(response.letter));
+
+        let letterPosition = wordParts.indexOf(response.letter);
+            // check if letter exists in word
+          if (letterPosition > -1) {
+            targetWord[letterPosition] = response.letter;
+            wordParts[letterPosition] = '-'
+
+            if (word == targetWord.join('')) {
+              guesses +=999;
+            }
+
+          }
+
+          
+
+          guesses++;
+          
+          console.log('Vārds: '+targetWord.join(' '))
+          console.log('')
+    }
+
+    if (word == targetWord.join('')) {
+        console.log("Correct");
+    } else {
+        console.log("You loose!");
+    }
+
+
+
+
+
+})();
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
+https://www.npmjs.com/package/prompts:
 git push -u origin main
